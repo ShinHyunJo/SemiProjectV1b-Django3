d2b725beb7f49e16f3bbc87ebd1efa64ea05bb41
glglgl3943@gmail.com <glglgl3943@gmail.com>
2022-06-21 12:37:44 +0900

    Django3 app urls.py, views  수정 - home, member, board

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('join/', views.list, name='join'),
+    path('login/', views.view, name='login'),
+    path('write', views.write, name='write'),
+]
