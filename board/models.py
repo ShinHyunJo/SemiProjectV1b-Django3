b88f1e39d8223d84267b1d70fc8edf88229003c9
glglgl3943@gmail.com <glglgl3943@gmail.com>
2022-06-24 11:57:13 +0900

    Django3 board 모델 생성, write.html 유효성 검사 추가

--- board/models.py
+++ board/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from datetime import datetime
+from member.models import Member
+
 
 # Create your models here.
+# on_delete : CASCADE, DO_NOTHING
+
+class board(models.Model):
+    id=models.AutoField(primary_key=True)
+    title=models.CharField(max_length=100)
+    userid=models.ForeignKey(Member,  db_column='userid',
+                             on_delete=models.CASCADE)   #외래키, 같이지워지게
+    regdate=models.DateTimeField(default=datetime.now)
+    views=models.IntegerField(default=0)
+    contents=models.TextField(null=False, blank=False)
+
+    class Meta:
+        db_table = 'board'
+        ordering = ['-regdate']
+
+
+    def __str__(self):
+        return self.title
+
